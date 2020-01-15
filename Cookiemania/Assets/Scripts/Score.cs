95cd311a63111ac71cd330a861ae333a062e8e0d
jwynd <18224844+jwynd@users.noreply.github.com>
2020-01-15 15:41:02 -0800

    Added sprite to indicate trust level. Add random 0 to 5 trust by pressing t. Money change function now tied to M. Changed object hierarchy

--- Cookiemania/Assets/Scripts/Score.cs
+++ Cookiemania/Assets/Scripts/Score.cs
@@ -8,18 +8,32 @@ public class Score : MonoBehaviour
     [HideInInspector] public int money = 100;
     [HideInInspector] public int trust = 0;
     private Text text;
+    private Image image;
     // Start is called before the first frame update
     void Start()
     {
-        text = this.gameObject.GetComponent<Text>();
+        text = this.transform.GetChild(0).gameObject.GetComponent<Text>();
+        image = this.transform.GetChild(1).gameObject.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = "$"+money;
-        if(Input.GetKeyDown(KeyCode.A)){
+        if(Input.GetKeyDown(KeyCode.M)){
             money += (int)Mathf.Floor((Random.value*10)-5);
+        }
+        if(Input.GetKeyDown(KeyCode.T)){
+            trust += (int)Mathf.Floor(Random.value*5);
+        }
+        if(trust < 20){
+            image.color = Color.black;
+        } else if(trust < 40){
+            image.color = Color.red;
+        } else if(trust < 60){
+            image.color = Color.green;
+        } else {
+            image.color = Color.white;
         }
     }
 }
