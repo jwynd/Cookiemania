11d03fb75acf745a493e5a3630633a3f009203df
jwynd <18224844+jwynd@users.noreply.github.com>
2020-01-15 14:38:16 -0800

    added new method of tracking money. Pressing A currently adds + or -

---
+++ Cookiemania/Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [HideInInspector] public int money = 100;
+    [HideInInspector] public int trust = 0;
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "$"+money;
+        if(Input.GetKeyDown(KeyCode.A)){
+            money += (int)Mathf.Floor((Random.value*10)-5);
+        }
+    }
+}
