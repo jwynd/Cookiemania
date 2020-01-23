edeee1abd07dcd19d4251bc701fccefa37f32022
Mia <alicie73@yahoo.com>
2020-01-22 16:04:16 -0800

    protectMiniGame
    Protect minigame template

---
+++ pminigame/Assets/scripts/Enemyfire.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyfire : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    public float speed = 30;
+
+    public Sprite PExplosionImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = Vector2.down * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "player")
+        {
+            //SoundManager.Instance.PlayOneShot(SoundManager.Instance.playerdeath);
+            col.GetComponent<SpriteRenderer>().sprite = PExplosionImage;
+            Destroy(gameObject);
+            DestroyObject(col.gameObject, 0.5f); //0.5f
+
+        }
+
+        if (col.tag == "shield") 
+         {
+             Destroy (gameObject);
+             DestroyObject(col.gameObject);
+         }
+
+    }
+    void OnBecomeInvisible()
+    {
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
