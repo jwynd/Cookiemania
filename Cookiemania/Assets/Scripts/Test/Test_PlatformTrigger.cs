e42f74339590cbfe8fb18752111ea5d09f3d4eb5
jwynd <18224844+jwynd@users.noreply.github.com>
2020-01-29 15:19:23 -0800

    Added platform generation and trigger scripts

---
+++ Cookiemania/Assets/Scripts/Test/Test_PlatformTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class Test_PlatformTrigger : MonoBehaviour
+{
+    public Test_PlatformGeneration gen;
+    
+    void OnTriggerEnter2D(Collider2D other){
+        gen.BuildSection();
+        // Debug.Log("Trigger Entered");
+        if(this.gameObject.name != gen.trigger.name) Destroy(this.gameObject);
+    }
+}
