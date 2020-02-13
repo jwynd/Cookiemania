095a4284f738f825b4c511044c2cc23eeaee9c28
jwynd <18224844+jwynd@users.noreply.github.com>
2020-02-12 18:10:16 -0800

    Work in progress: email system

---
+++ Cookiemania/Assets/Scripts/Desktop/Desktop_EmailController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class Desktop_EmailController : MonoBehaviour
+{
+    public static Desktop_EmailController Instance;
+    public Vector3 nextEmailPosition;
+    public GameObject emailPrefab;
+    public float offset;
+    private static List<GameObject> emails = new List<GameObject>();
+
+    void OnValidate(){
+        Assert.IsNotNull(emailPrefab);
+        Assert.IsNotNull(emailPrefab.transform.GetChild(0).gameObject.GetComponent<Text>());
+        Assert.IsNotNull(emailPrefab.GetComponent<General_Email>());
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(Instance != null && Instance != this){
+          Debug.LogError("Attempting to create second instance of Singleton pattern: Desktop_EmailController");
+          Destroy(this);
+        }
+        Instance = this;
+        emailPrefab.SetActive(false);
+    }
+
+    public void spawnEmail(string subject, string body){
+        GameObject e = Instantiate(emailPrefab, Vector3.zero, Quaternion.identity);
+        e.transform.GetChild(0).gameObject.GetComponent<Text>().text = subject;
+        e.GetComponent<General_Email>().bodyText = body;
+        e.GetComponent<RectTransform>().position = nextEmailPosition;
+        e.SetActive(true);
+        emails.Add(e);
+        nextEmailPosition = new Vector3(nextEmailPosition.x, nextEmailPosition.y - offset, nextEmailPosition.z);
+    }
+
+    // for testing purposes only
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+          spawnEmail("Test Email", "This is a test");
+        }
+    }
+}
