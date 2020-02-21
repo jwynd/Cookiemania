cb7332cb24bd3c6606f02039050cda2e1b3f5ca4
jwynd <18224844+jwynd@users.noreply.github.com>
2020-02-20 20:10:14 -0800

    Emails can spawn anytime and are only visible when the email page is active

---
+++ Cookiemania/Assets/Scripts/General/Utility_MatchActive.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility_MatchActive : MonoBehaviour
+{
+    public GameObject match;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(match == null){
+            Debug.LogError("Match is null, Destroying self");
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void OnEnable()
+    {
+        match.SetActive(true);
+    }
+
+    void OnDisable()
+    {
+        match.SetActive(false);
+    }
+}
