e1708c62a7adc44fe5284e86ad888f54b410c282
jwynd <18224844+jwynd@users.noreply.github.com>
2020-04-03 12:52:00 -0700

    Finished basic email system. Bug fix in email ordering

--- Cookiemania/Assets/Scripts/Desktop/Desktop_EmailController.cs
+++ Cookiemania/Assets/Scripts/Desktop/Desktop_EmailController.cs
@@ -9,15 +9,19 @@ public class Desktop_EmailController : MonoBehaviour
 {
     public static Desktop_EmailController Instance;
     public Vector3 nextEmailPosition;
-    public GameObject emailPrefab;
+    public GameObject newEmail;
+    public GameObject emailLiteral;
     public float offset;
+    public GameObject[] disableOnEmailClick;
+    public GameObject[] enableOnEmailClick;
     private GameObject emailsParent;
     private static List<GameObject> emails = new List<GameObject>();
+    private Vector3 originalEmailPos;
 
     void OnValidate(){
-        Assert.IsNotNull(emailPrefab);
-        Assert.IsNotNull(emailPrefab.transform.GetChild(0).gameObject.GetComponent<Text>());
-        Assert.IsNotNull(emailPrefab.GetComponent<General_Email>());
+        Assert.IsNotNull(newEmail);
+        // Assert.IsNotNull(emailPrefab.transform.GetChild(0).gameObject.GetComponent<Text>());
+        Assert.IsNotNull(newEmail.GetComponent<Email_Click>());
     }
 
     // Start is called before the first frame update
@@ -28,25 +32,54 @@ public class Desktop_EmailController : MonoBehaviour
           Destroy(this);
         }
         Instance = this;
-        emailsParent = GameObject.Find("Canvas/EmailsParent");
-        emailPrefab.SetActive(false);
+        emailsParent = GameObject.Find("Email");
+        originalEmailPos = nextEmailPosition;
     }
 
-    public void spawnEmail(string subject, string body, string[] responses, UnityAction[] responseActions){
-        GameObject e = Instantiate(emailPrefab, Vector3.zero, Quaternion.identity);
-        e.transform.GetChild(0).gameObject.GetComponent<Text>().text = subject;
-        e.GetComponent<General_Email>().bodyText = body;
-        e.GetComponent<RectTransform>().position = nextEmailPosition;
+    public void spawnEmail(string subject, string body, string[] responseNames, UnityAction[] responseActions){
+        GameObject e = Instantiate(newEmail, nextEmailPosition, Quaternion.identity);
         e.transform.SetParent(emailsParent.transform, false);
         e.SetActive(true);
         emails.Add(e);
         nextEmailPosition = new Vector3(nextEmailPosition.x, nextEmailPosition.y - offset, nextEmailPosition.z);
+        Email_Click ec = e.GetComponent<Email_Click>();
+        ec.disable = disableOnEmailClick;
+        ec.enable = enableOnEmailClick;
+        ec.email = emailLiteral;
+        ec.names = responseNames;
+        ec.responses = responseActions;
+        ec.subject = subject;
+        ec.body = body;
+    }
+
+    // this function should be called in Email_Clicks onDestroy
+    public void reorderEmails(){
+      // Debug.LogError("Reorder emails is not implemented");
+      nextEmailPosition = originalEmailPos;
+      foreach(GameObject e in emails){
+        if(e == null) continue;
+        e.transform.position = nextEmailPosition;
+        nextEmailPosition = new Vector3(nextEmailPosition.x, nextEmailPosition.y - offset, nextEmailPosition.z);
+      }
+    }
+
+    void money(){
+      General_Score.Instance.money += 20;
+    }
+
+    void trust(){
+      General_Score.Instance.trust += 20;
+    }
+
+    void both(){
+      money();
+      trust();
     }
 
     // for testing purposes only
-    // void Update(){
-    //     if(Input.GetKeyDown(KeyCode.Space)){
-    //       spawnEmail("Test Email", "This is a test");
-    //     }
-    // }
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+          spawnEmail("Test Email", "This is a test", new string[] {"Money", "Trust", "Both"}, new UnityAction[] {money, trust, both});
+        }
+    }
 }
--- Cookiemania/Assets/Scripts/Desktop/Email_Click.cs
+++ Cookiemania/Assets/Scripts/Desktop/Email_Click.cs
@@ -2,30 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Email_Click : MonoBehaviour
 {
-    public GameObject[] disable;
-    public GameObject[] enable;
-    public Email email;
-    private void adjustMoney(){
-        General_Score.Instance.money += 10;
-    }
-    private void adjustTrust(){
-        General_Score.Instance.trust += 21;
-    }
-    private void adjustBoth(){
-        adjustMoney();
-        adjustTrust();
-    }
-    
-    void Awake(){
-
-    }
+    [HideInInspector] public GameObject[] disable;
+    [HideInInspector] public GameObject[] enable;
+    [HideInInspector] public GameObject email;
+    [HideInInspector] public string[] names;
+    [HideInInspector] public UnityAction[] responses;
+    [HideInInspector] public string subject;
+    [HideInInspector] public string body;
 
     void OnMouseDown(){
         // Debug.Log("Clicked");
-        email.formatEmail(this.gameObject, disable, enable,new string[] {"Money", "Trust", "Both"} ,new UnityAction[] {adjustMoney, adjustTrust, adjustBoth});
+        GameObject e_go = Instantiate(email, Vector3.zero, Quaternion.identity);
+        e_go.transform.SetParent(GameObject.Find("Canvas").transform, false);
+        e_go.SetActive(true);
+        Text s = e_go.transform.GetChild(1).GetComponent<Text>();
+        s.text = subject;
+        Text b = e_go.transform.GetChild(2).GetComponent<Text>();
+        b.text = body;
+        Email e = e_go.GetComponent<Email>();
+        e.formatEmail(this.gameObject, disable, enable, names, responses);
+        //email.formatEmail(this.gameObject, disable, enable, names, responses);
     }
     
     void OnDestroy(){
@@ -35,5 +35,6 @@ public class Email_Click : MonoBehaviour
         foreach(GameObject e in enable){
             Destroy(e);
         }
+        Desktop_EmailController.Instance.reorderEmails();
     }
 }
