1b6eb987779d189b5c4bfde299d4af9c0e46dc68
jwynd <18224844+jwynd@users.noreply.github.com>
2020-04-22 15:09:21 -0700

    Added in settings menu and audio mixer

--- Cookiemania/Assets/Scripts/Menus/MainMenu.cs
+++ Cookiemania/Assets/Scripts/Menus/MainMenu.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject settingsPrefab;
+    
     public void Play(){
         SceneManager.LoadScene("Desktop", LoadSceneMode.Single);
+    }
+
+    public void Settings(){
+        // specific for main menu
+        Transform c = GameObject.Find("Canvas").transform;
+        List<GameObject> g = new List<GameObject>();
+        
+        foreach(Transform child in c){
+            g.Add(child.gameObject);
+            child.gameObject.SetActive(false);
+        }
+        // needed to spawn the pause menu
+        GameObject s = Instantiate(settingsPrefab);
+        s.transform.parent = c;
+        s.transform.position = new Vector3(Screen.width*0.5f, Screen.height*0.5f, 0);
+        s.GetComponent<SettingsMenu>().activateOnDestroy = g.ToArray();
+        s.SetActive(true);
     }
 }
---
+++ Cookiemania/Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject[] activateOnDestroy;
+    public AudioMixer mainMixer;
+    // Update is called once per frame
+    
+    void Awake(){
+        // mainMixer = Resources.Load("Master") as AudioMixer;
+        // if(mainMixer == null){
+        //     Debug.LogError("Audio Mixer not found");
+        // }
+        Slider[] sliders = GetComponentsInChildren<Slider>();
+        // 10^(mixerval / 20) = sliderval
+        float musicVolume;
+        float sfxVolume;
+        mainMixer.GetFloat("musicVolume", out musicVolume);
+        mainMixer.GetFloat("sfxVolume", out sfxVolume);
+        sliders[0].value = Mathf.Pow(10, (musicVolume / 20));
+        sliders[1].value = Mathf.Pow(10, (sfxVolume / 20));
+    }
+
+    public void SetSFXVolume(float sfxSliderVal){
+        mainMixer.SetFloat("sfxVolume", Mathf.Log10(sfxSliderVal) * 20);
+    }
+    
+    public void SetMusicVolume(float musicSliderVal){
+        mainMixer.SetFloat("musicVolume", Mathf.Log10(musicSliderVal) * 20);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnDestroy(){
+        foreach(GameObject g in activateOnDestroy){
+            g.SetActive(true);
+        }
+    }
+}
