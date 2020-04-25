072b2112c0046ad1c0bc9dd58c51c1faf1a49a7b
Matthew Stevens <marsteve@ucsc.edu>
2020-04-24 17:11:36 -0700

    wow asset city... lol

---
+++ Cookiemania/Assets/JumperResources/Scripts/JumperBackgroundController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperBackgroundController : MonoBehaviour
+{
+    [SerializeField]
+    protected List<GameObject> dayPrefabs;
+    [SerializeField]
+    protected List<GameObject> nightPrefabs;
+    protected JumperManagerGame gm;
+    void Start()
+    {
+        gm = JumperManagerGame.Instance;
+        SetNight(gm.Night);
+    }
+
+    private void SetNight(bool night)
+    {
+        foreach (var obj in dayPrefabs)
+        {
+            obj.SetActive(!night);
+        }
+        foreach (var obj in nightPrefabs)
+        {
+            obj.SetActive(night);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Cookiemania/Assets/JumperResources/Scripts/JumperManagerGame.cs
+++ Cookiemania/Assets/JumperResources/Scripts/JumperManagerGame.cs
@@ -7,8 +7,8 @@ public class JumperManagerGame : MonoBehaviour
 {
 
     #region variables
-    [System.Serializable]
-    public class Prefabs
+    [Serializable]
+    public class Platforms
     {
         [Tooltip("Spawnable object prefabs")]
         public GameObject platform = null;
@@ -16,17 +16,21 @@ public class JumperManagerGame : MonoBehaviour
             "the array are more likely due to using pseudo normal distribution.")]
         [Range(0.01f, 1.0f)]
         public float weight = 0.1f;
-        [HideInInspector]
-        public float probability = 0f;
+        public float probability { get; set; } = 0f;
     }
 
-
-    
     [SerializeField]
-    private Prefabs[] platformPrefabs = null;
-
+    private List<Platforms> nightPrefabs = new List<Platforms>();
     [SerializeField]
-    private JumperSemiPermanentPlatforms exitPlatform = null;
+    private List<Platforms> dayPrefabs = new List<Platforms>();
+    [SerializeField]
+    private GameObject nightExit = null;
+    [SerializeField]
+    private GameObject dayExit = null;
+    [SerializeField]
+    private JumperSemiPermanentPlatforms startingPlatform = null;
+    [SerializeField]
+    private Sprite defaultDayPlatformImage = null;
 
     [Tooltip("How far to either side from the offset position a platform can spawn")]
     public float width = 10.0f;
@@ -49,17 +53,28 @@ public class JumperManagerGame : MonoBehaviour
     public float rotation = 0.0f;
     public float alteredGravity = 2f;
     public float bounceHeightMultiplier = 3f;
+    [SerializeField]
     [Tooltip("Important game tag, ensure this is added to the tags list")]
-    public string playerTag = "Player";
+    protected string playerTag = "Player";
+    public string PlayerTag { get { return playerTag; } }
+    [SerializeField]
     [Tooltip("Important game tag, ensure this is added to the tags list")]
-    public string enemyTag = "Enemy";
+    protected string enemyTag = "Enemy";
+    public string EnemyTag { get { return playerTag; } }
+    [SerializeField]
     [Tooltip("Important game tag, ensure this is added to the tags list")]
-    public string obstacleTag = "Obstacle";
+    protected string obstacleTag = "Obstacle";
+    public string ObstacleTag { get { return obstacleTag; } }
+    [SerializeField]
     [Tooltip("Important game tag, ensure this is added to the tags list")]
-    public string groundTag = "Platform";
+    protected string groundTag = "Platform";
+    public string GroundTag { get { return groundTag; } }
+    [SerializeField]
     [Tooltip("Important game tag, ensure this is added to the tags list")]
-    public string collectiblesTag = "Pickup";
-    public bool runTutorial = true;
+    protected string collectiblesTag = "Pickup";
+    public string CollectiblesTag { get { return collectiblesTag; } }
+    public bool Tutorial { get; protected set; } = true;
+    public bool Night { get; protected set; } = true;
 
     //game manager always tagged with game controller
     private const string myTag = "GameController";
@@ -75,6 +90,8 @@ public class JumperManagerGame : MonoBehaviour
     private float weightRange = 0.0f;
     private float checkAgainstPosition;
     private float jumpHeight;
+    private List<Platforms> actualPrefabs;
+    private GameObject actualExit;
 
     public JumperCameraController MainCam { get; private set; }
 
@@ -85,6 +102,7 @@ public class JumperManagerGame : MonoBehaviour
     #endregion
 
     #region startup
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -95,17 +113,38 @@ public class JumperManagerGame : MonoBehaviour
         {
             Instance = this;
         }
+        //look to actual game manager to update outer game variables and stuff
+        RunAwakeInit();
+    }
+
+    private void RunAwakeInit()
+    {
         weightRange = 0;
-        foreach (var fab in platformPrefabs)
-        {
-            fab.probability = fab.weight + weightRange;
-            weightRange += fab.weight;
-        }
+        PrefabInit();
         Player = FindObjectOfType<JumperPlayerController>().gameObject.GetComponent<JumperPlayerController>();
         MainCam = FindObjectOfType<JumperCameraController>().gameObject.GetComponent<JumperCameraController>();
         heightGoal *= startingDifficulty;
     }
 
+    private void PrefabInit()
+    {
+        if (Night)
+        {
+            actualPrefabs = nightPrefabs;
+            actualExit = nightExit;
+        }
+        else
+        {
+            actualPrefabs = dayPrefabs;
+            actualExit = dayExit;
+            startingPlatform.gameObject.GetComponent<SpriteRenderer>().sprite = defaultDayPlatformImage;
+        }
+        foreach (var fab in actualPrefabs)
+        {
+            fab.probability = fab.weight + weightRange;
+            weightRange += fab.weight;
+        }
+    }
 
     void OnValidate() 
     {
@@ -117,21 +156,21 @@ public class JumperManagerGame : MonoBehaviour
 
     private void ValidationChecks()
     {
-        if (!Player || !MainCam || platformPrefabs.Length == 0)
+        if (!Player || !MainCam || actualPrefabs.Count == 0)
         {
             Debug.LogError("need a player, platform prefabs and camera with appropriate scripts" +
                 " instantiated in the scene");
         }
-        if (platformPrefabs[0].platform == null)
+        if (actualPrefabs[0].platform == null)
         {
             Debug.LogError("Platforms should not be null references");
         }
         gameObject.tag = myTag;
-        TagExists(playerTag);
-        TagExists(groundTag);
-        TagExists(enemyTag);
-        TagExists(obstacleTag);
-        TagExists(collectiblesTag);
+        TagExists(PlayerTag);
+        TagExists(GroundTag);
+        TagExists(EnemyTag);
+        TagExists(ObstacleTag);
+        TagExists(CollectiblesTag);
         gameObject.tag = myTag;
     }
 
@@ -147,7 +186,7 @@ public class JumperManagerGame : MonoBehaviour
     void Start()
     {
         ValidationChecks();
-        GameObject g = Instantiate(platformPrefabs[0].platform, transform.position, Quaternion.identity);
+        GameObject g = Instantiate(actualPrefabs[0].platform, transform.position, Quaternion.identity);
         firstPrefabWidth = g.GetComponent<Renderer>().bounds.size.x;
         minHeightIncrease = Player.gameObject.GetComponent<Renderer>().bounds.size.y * 2f + g.GetComponent<Renderer>().bounds.size.y;
         Destroy(g);
@@ -164,16 +203,16 @@ public class JumperManagerGame : MonoBehaviour
     private GameObject Roulette()
     {
         //the 0 possibility is caught on startup
-        if (platformPrefabs.Length < 2) { return platformPrefabs[0].platform; }
+        if (actualPrefabs.Count < 2) { return actualPrefabs[0].platform; }
         float random = UnityEngine.Random.Range(0, weightRange/2) + UnityEngine.Random.Range(0, weightRange/2);
-        foreach (var fab in platformPrefabs)
+        foreach (var fab in actualPrefabs)
         {
             if (random < fab.probability)
             {
                 return fab.platform;
             }
         }
-        return platformPrefabs[platformPrefabs.Length - 1].platform;
+        return actualPrefabs[actualPrefabs.Count - 1].platform;
     }
 
     #endregion
@@ -205,7 +244,7 @@ public class JumperManagerGame : MonoBehaviour
         float randomx = BuildHelper();
         float randomy = UnityEngine.Random.Range(height + minHeightIncrease, height + jumpHeight);
         Vector3 pos = new Vector3(randomx, randomy, 0);
-        Instantiate(exitPlatform, pos, Quaternion.Euler(0, 0, rotation));
+        Instantiate(actualExit, pos, Quaternion.Euler(0, 0, rotation));
         haveBuiltExit = true;
     }
 
@@ -286,27 +325,27 @@ public class JumperManagerGame : MonoBehaviour
 
     public string GetPlayerTag()
     {
-        return playerTag; 
+        return PlayerTag; 
     }
 
     public string GetEnemyTag()
     {
-        return enemyTag;
+        return EnemyTag;
     }
 
     public string GetObstacleTag()
     {
-        return obstacleTag;
+        return ObstacleTag;
     }
 
     public string GetGroundTag()
     {
-        return groundTag;
+        return GroundTag;
     }
 
     public string GetCollectiblesTag()
     {
-        return collectiblesTag;
+        return CollectiblesTag;
     }
 
     public float GetAlteredGravity()
@@ -316,7 +355,7 @@ public class JumperManagerGame : MonoBehaviour
 
     public bool IsRunningTutorial()
     {
-        return runTutorial;
+        return Tutorial;
     }
 
     //credit: https://forum.unity.com/threads/re-map-a-number-from-one-range-to-another.119437/
