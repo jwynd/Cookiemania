abda28f99c72599ef8bbbf3836459e661529ee40
jwynd <18224844+jwynd@users.noreply.github.com>
2020-06-02 13:33:53 -0700

    Added test scene with first draft conversations

---
+++ Cookiemania/Assets/Scripts/Chat/Chat_PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chat_PlayerMove : MonoBehaviour
+{
+    private bool canMove = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove && Input.GetKeyDown(KeyCode.Mouse0)){
+            this.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        }
+    }
+
+    public void ForbidMove(){
+        canMove = false;
+    }
+
+    public void AllowMove(){
+        canMove = true;
+    }
+}
