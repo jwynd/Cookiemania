34d09da6d9fe097376aa8f12c936be653bcff493
Mia <alicie73@yahoo.com>
2020-11-03 04:47:43 -0800

    BackgroundController gameobject + script outling in progress
    outline of the script

---
+++ Cookiemania/Assets/Scripts/Chat/BackgroundController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public delegate void function();
+    public function m_call;
+    public Transform parentTransform;
+    void Start()
+    {
+        //Fill Delegate
+        //m_call = whateverwefillwith;
+    }
+
+    public void Init()
+    {
+        for(int i = 0; i < parentTransform.childCount; i++)
+        {
+            parentTransform.GetChild(i).gameObject.SetActive(true);
+        }
+
+        SetStage(0, m_call);
+    }
+
+    public void SetStage(int v, function m_call)
+    {
+        /*needs a public void SetStage(int v, Function<> callback) function that sets
+        the scene to stage v and invokes the given callback when its ready for the next stage e.g.
+        the movement has completed*/
+    }
+
+    public bool CanSetStage(int v)
+    {
+        // function that says whether a given v has a stage defined for it
+        return true;
+    }
+}
