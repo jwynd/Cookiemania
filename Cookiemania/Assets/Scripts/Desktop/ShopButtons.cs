372bc028951bcb174b99f7622ecdfdb8eb9b0810
Mia <alicie73@yahoo.com>
2020-11-17 18:05:22 -0800

    Analytics tabs done
    Analytics tabs layouts and functionality with switching between now done. Functionality and replacement assets still needed

--- Cookiemania/Assets/Scripts/Desktop/ShopButtons.cs
+++ Cookiemania/Assets/Scripts/Desktop/ShopButtons.cs
@@ -9,24 +9,71 @@ public class ShopButtons : MonoBehaviour
     string mini; //either cyber or marketing
     public int lvlReq;
     private bool purchased = false;
-    public GameObject global;
-    public GameObject marketing;
-    public GameObject cyber;
+    [SerializeField]
+    protected GameObject global = null;
+    [SerializeField]
+    protected GameObject marketing = null;
+    [SerializeField]
+    protected GameObject cyber = null;
 
+    protected Canvas controlledCanvas1;
+    protected Canvas controlledCanvas2;
+    protected Canvas controlledCanvas3;
+    protected Canvas controlledCanvas4;
+    protected GameObject controlledObj1;
+    protected GameObject controlledObj2;
+    protected GameObject controlledObj3;
+    protected GameObject controlledObj4;
+
+    private void Awake()
+    {
+        controlledObj4 = GameObject.Find("ShopCanvas(Clone)");
+        controlledCanvas4 = controlledObj4.GetComponent<Canvas>();
+        if (gameObject.name == "ShopCanvas(Clone)")
+        {
+            controlledObj1 = Instantiate(global);
+            controlledCanvas1 = controlledObj1.GetComponent<Canvas>();         
+
+            controlledObj2 = Instantiate(marketing);
+            controlledCanvas2 = controlledObj2.GetComponent<Canvas>();        
+
+            controlledObj3 = Instantiate(cyber);
+            controlledCanvas3 = controlledObj3.GetComponent<Canvas>();
+
+            Debug.Log("Is Shop Canvas");
+        }
+        else
+        {
+            Debug.Log("NotShop Canvas");
+        }              
+    }
     private void Start()
     {
-        //if (PlayerData.Player.money >= cost && PlayerData.Player.shoplvl >= lvlReq && PlayerData.Player.SpaceUpgradelvl >= lvlReq && purchased == false)
-       // {
-        //    gameObject.GetComponent<Image>().color = Color.yellow;
-        //}
+        if (gameObject.name == "ShopCanvas(Clone)")
+        {
+            controlledObj1.transform.SetParent(SiteCanvas.Instance.transform);
+            controlledObj2.transform.SetParent(SiteCanvas.Instance.transform);
+            controlledObj3.transform.SetParent(SiteCanvas.Instance.transform);
+        }
+        if (gameObject.name != "ShopCanvas(Clone)")
+        {
+            controlledObj1 = GameObject.Find("Global(Clone)");
+            controlledCanvas1 = controlledObj1.GetComponent<Canvas>();
+            controlledObj2 = GameObject.Find("Marketing(Clone)");
+            controlledCanvas2 = controlledObj2.GetComponent<Canvas>();
+            controlledObj3 = GameObject.Find("Cyber(Clone)");
+            controlledCanvas3 = controlledObj3.GetComponent<Canvas>();
+        }
+        if (GameObject.Find("Cyber(Clone)"))
+        {
+            controlledCanvas1.enabled = false;
+            controlledCanvas2.enabled = false;
+            controlledCanvas3.enabled = false;
+        }
     }
 
     private void Update()
     {
-        if (purchased == true)
-        {
-            gameObject.GetComponent<Image>().color = Color.black;
-        }
     }
     public void Buy()
     {
@@ -80,39 +127,33 @@ public class ShopButtons : MonoBehaviour
 
     public void Global()
     {
-        global.SetActive(true);
-        marketing.SetActive(false);
-        cyber.SetActive(false);
-        this.gameObject.SetActive(false);
+        controlledCanvas1.enabled = true;
+        controlledCanvas2.enabled = false;
+        controlledCanvas3.enabled = false;
+        controlledCanvas4.enabled = false;
     }
 
     public void Marketing()
     {
-        global.SetActive(false);
-        marketing.SetActive(true);
-        cyber.SetActive(false);
-        this.gameObject.SetActive(false);
+        controlledCanvas1.enabled = false;
+        controlledCanvas2.enabled = true;
+        controlledCanvas3.enabled = false;
+        controlledCanvas4.enabled = false;
     }
 
     public void Cyber()
     {
-        global.SetActive(true);
-        marketing.SetActive(false);
-        cyber.SetActive(false);
-        this.gameObject.SetActive(false);
+        controlledCanvas1.enabled = false;
+        controlledCanvas2.enabled = false;
+        controlledCanvas3.enabled = true;
+        controlledCanvas4.enabled = false;
     }
 
     public void Back()
-    {
-        if (this.gameObject.activeSelf != true)
-        {
-            global.SetActive(false);
-            marketing.SetActive(false);
-            cyber.SetActive(false);
-            this.gameObject.SetActive(true);
-        } else
-        {
-            //home tab?
-        }
+    {    
+            controlledCanvas1.enabled = false;
+            controlledCanvas2.enabled = false;
+            controlledCanvas3.enabled = false;
+            controlledCanvas4.enabled = true;
     }
 }
