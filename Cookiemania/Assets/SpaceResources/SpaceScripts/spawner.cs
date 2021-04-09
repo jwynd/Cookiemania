93bce87ef7abdba4322a7e71f89705bb8fc57814
Mia <alicie73@yahoo.com>
2021-04-09 15:21:43 -0700

    spawner balancing

--- Cookiemania/Assets/SpaceResources/SpaceScripts/spawner.cs
+++ Cookiemania/Assets/SpaceResources/SpaceScripts/spawner.cs
@@ -12,8 +12,9 @@ public class spawner : MonoBehaviour
     List<GameObject> Enemies = new List<GameObject>();
     float randX;
     Vector2 whereToSpawn;
-    public float spawnRate = 5f;
+    public float spawnRate = 10f;
     float nextSpawn = 1f;
+    float probability;
 
 
     // Start is called before the first frame update
@@ -23,10 +24,31 @@ public class spawner : MonoBehaviour
         Enemies.Add(enemy2);
         Enemies.Add(enemy3);
         Enemies.Add(enemy4);
+
+        switch (PlayerData.Player.spacelvl)
+        {
+            case 1:
+               
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        spawnAlgorithmThree();
+    }
+
+    void spawnAlgorithmOne() //spawner guaranteed to spawn at spawn time for every spawner
     {
         if (Time.time > nextSpawn)
         {
@@ -38,4 +60,44 @@ public class spawner : MonoBehaviour
             eTemp.transform.parent = null;
         }
     }
+
+    void spawnAlgorithmTwo()// spawner has 70 percent chance to spawn an enemy on time for every spawner.
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-14.4f, 14.4f);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            probability = Random.Range(0, 100);
+            if (probability <= 70)
+            {
+                GameObject eTemp = Instantiate((Enemies[Random.Range(0, Enemies.Count)]), whereToSpawn, Quaternion.identity);
+                eTemp.transform.parent = transform;
+                eTemp.transform.parent = null;
+            }
+        }
+    }
+
+    void spawnAlgorithmThree()// spawner has 50 percent chance to spawn and enemy on time for each spawn site.
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-14.4f, 14.4f);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            probability = Random.Range(0, 100);
+            Debug.Log(probability);
+            if (probability <= 50)
+            {
+                GameObject eTemp = Instantiate((Enemies[Random.Range(0, Enemies.Count)]), whereToSpawn, Quaternion.identity);
+                eTemp.transform.parent = transform;
+                eTemp.transform.parent = null;
+            }
+            else
+            {
+                Debug.Log("no spawn this turn");
+            }
+        }
+    }
+
 }
