69c6978fb5133826936619c6ae8cce02eed28496
Matthew Stevens <marsteve@ucsc.edu>
2021-06-28 20:58:20 -0700

    ui on player / bottom of screen

---
+++ Cookiemania/Assets/Scripts/General/Unflipper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unflipper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SpriteRenderer parentRend;
+    private float lastFlip = 1f;
+    void Start()
+    {
+        parentRend = transform.parent.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (parentRend.transform.localScale.x * lastFlip < 0f)
+        {
+            lastFlip = parentRend.transform.localScale.x;
+            transform.localScale = new Vector3(
+                -transform.localScale.x, 
+                transform.localScale.y, 
+                transform.localScale.z);
+            transform.localPosition = new Vector3(
+                -transform.localPosition.x, 
+                transform.localPosition.y, 
+                transform.localPosition.z);
+        }
+    }
+}
